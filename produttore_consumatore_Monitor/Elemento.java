cdd62062c05418edba9624092325bcc517469051
Alessandra <alessandra.p2001@gmail.com>
2023-12-31 12:49:14 +0100

    Produttore_Consumatore_Monitor

---
+++ produttore_consumatore_Monitor/Elemento.java
@@ -0,0 +1,19 @@
+package produttore_consumatore_Monitor;
+
+public class Elemento {
+    private int i;
+
+	public Elemento(int i) {
+		super();
+		this.i = i;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+    
+}
